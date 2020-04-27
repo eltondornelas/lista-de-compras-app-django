4994e9edad23ad6604691727ebf09cf3f3ed7610
Elton Dornelas <eltondornelas@gmail.com>
2020-04-26 23:52:25 -0300

    Criação do Projeto e dos Apps

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/elton/workspace/ws-django/lista-de-compras-app-django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
